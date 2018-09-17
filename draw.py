8c94a9df2b2aa98130492a22fe93c552b8d1421f
xiaojianliu <xiaojianliu2016@gmail.com>
2018-09-17 12:20:12 -0400

    Update draw.py

--- draw.py
+++ draw.py
@@ -5,11 +5,8 @@ Created on Tue Dec 20 10:18:02 2016
 @author: xiaojian
 """
 import numpy as np
-import datetime as dt
-import pandas as pd
-from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
-import csv
+
 drifter_list='drifter_vs_roms use hourly data152004b.csv'
 #mend_list='bar1.csv'
 day=15
@@ -108,83 +105,3 @@ axes[1].plot(x1,num1,'r-',label='fvcom',linewidth=1)
 axes[1].legend(loc='best')
 plt.savefig('roms_vs_fvcomx%shah'%day,dpi=200)
 plt.show()
-print 'mean',np.mean(drifters['s'])/day
-print 'max',np.max(drifters['s'])
-print 'min',np.min(drifters['s'])
-print 'std',np.std(drifters['s'])
-#print np.mean(drifters['s'][drifters['d']<1])
-print 'mean',np.mean(drifters['d'])
-print 'max',np.max(drifters['d'])
-print 'min',np.min(drifters['d'])
-print 'std',np.std(drifters['d'])
-plt.figure()
-#plt.title('drifter_meandis_and_mend_meandis') 
-#plt.scatter(drifters['lon'][drifters['s']>2*np.mean(drifters['s'])],drifters['lat'][drifters['s']>2*np.mean(drifters['s'])],s=drifters['s'][drifters['s']>2*np.mean(drifters['s'])]*2,color='red')
-plt.scatter(drifters['lon'][drifters['s']>10],drifters['lat'][drifters['s']>10],s=drifters['s'][drifters['s']>10]*2,color='red')
-
-#plt.scatter(drifters['lon'],drifters['lat'],s=drifters['d']*10,color='yellow')
-plt.show()
-plt.figure()
-#plt.title('drifter_meandis_and_mend_meandis') 
-#plt.scatter(drifters['lon'],drifters['lat'],s=drifters['s'],color='red')
-#plt.scatter(drifters['lon'][drifters['d']>2*np.mean(drifters['d'])],drifters['lat'][drifters['d']>2*np.mean(drifters['d'])],s=drifters['d'][drifters['d']>2*np.mean(drifters['d'])]*20,color='green')
-plt.scatter(drifters['lon'][drifters['d']>0.4],drifters['lat'][drifters['d']>0.4],s=drifters['d'][drifters['d']>0.4]*20,color='green')
-
-plt.show()
-i=0
-I=[]
-xx=[]
-for a in np.arange(len(num1)):
-    if a%2==0 and a!=142:
-        i=num1[a]+i
-        I.append(i/float((sum(num1)/2)))
-for a in np.arange(len(x1)):
-    if a%2==0 and a!=142:
-        
-        xx.append((x1[a]+x1[a+1])/float(2))
-plt.figure()
-plt.plot(xx,I)
-II=[]
-for a in np.arange(len(I)):
-    II.append(1-I[a])
-plt.figure()
-plt.plot(xx,II)
-mm=[]
-for a in np.arange(len(num1)):
-    if a%2==0 and a!=142:
-        mm.append(num1[a]/float(sum(num1)/float(2)))
-plt.figure()
-plt.plot(xx,mm)
-        
-'''
-dd=[]
-dd=drifters['s'][mend['s']>7]-mend['s'][mend['s']>7]
-print len(dd)
-print len(dd[dd<=0])
-new_drifters=[]
-new_drifters=drifters['s']
-drifters['s'][mend['s']<7]=mend['s'][mend['s']<7]
-#new_drifters.append(drifters['s'][mend['s']>=10])
-num=[]
-x=[]
-for b in np.arange(1,round(max(drifters['s']))+1):
-    #print b,'##################################'
-    aa=0
-    x.append(b-1)
-    x.append(b)
-    for a in np.arange(len(drifters['s'])):
-        if drifters['s'][a]>=b-1 and drifters['s'][a]<b:
-            aa=aa+1
-            #print drifters['s'][a]
-    num.append(aa)
-    num.append(aa)
-num.append(0)
-x.append(b)
-plt.figure()
-#plt.title('drifter_meandis_and_mend_meandis') 
-plt.plot(x,num,'b-')
-plt.show()
-print 'mean',np.mean(drifters['s'])
-print 'max',np.max(drifters['s'])
-print 'min',np.min(drifters['s'])
-'''
